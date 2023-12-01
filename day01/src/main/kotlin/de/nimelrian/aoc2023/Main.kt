684bf31386eeafed0a10e3a3d3e14902070dfd2d
Sebastian Kappen <sebastian@kappen.dev>
2023-12-01 13:39:24 +0100

    Generify day 1 solution, add day 1 part 2

--- day01/src/main/kotlin/de/nimelrian/aoc2023/Main.kt
+++ day01/src/main/kotlin/de/nimelrian/aoc2023/Main.kt
@@ -2,21 +2,47 @@ package de.nimelrian.aoc2023
 
 import de.nimelrian.aoc.aoc
 
+val digitsToDigits = (0..10).associateBy { it.toString() }
+val wordsToDigits = mapOf(
+    "one" to 1,
+    "two" to 2,
+    "three" to 3,
+    "four" to 4,
+    "five" to 5,
+    "six" to 6,
+    "seven" to 7,
+    "eight" to 8,
+    "nine" to 9,
+)
+
 fun main() = aoc {
     part(solver = ::solve1)
+    part(solver = ::solve2)
 }
 
-fun solve1(lines: Sequence<String>) =
-    lines.map { line ->
-        val (firstDigit, lastDigit) = line.asSequence()
-            .mapNotNull { it.digitToIntOrNull() }
-            .fold(Pair<Int?, Int?>(null, null)) { (first, _), next ->
-                Pair(first ?: next, next)
-            }
+fun solve1(lines: Sequence<String>) = lines.map(CalibrationValueParser(digitsToDigits)).sum()
 
-        requireNotNull(firstDigit)
-        requireNotNull(lastDigit)
+fun solve2(lines: Sequence<String>) = lines.map(CalibrationValueParser(digitsToDigits + wordsToDigits)).sum()
 
-        firstDigit * 10 + lastDigit
+class CalibrationValueParser(private val digitLookupTable: Map<String, Int>) : (String) -> Int {
+    override fun invoke(line: String): Int = parseNumberFromLine(line)
+
+    private fun parseNumberFromLine(line: String): Int {
+        val (firstDigit, lastDigit) = parseEnclosingDigitsFromLine(line)
+        return firstDigit * 10 + lastDigit
     }
-        .sum()
+
+    private fun parseEnclosingDigitsFromLine(line: String): Pair<Int, Int> {
+        val needles = digitLookupTable.keys
+        val first = line.findAnyOf(needles)?.second?.let(::toDigit)
+        val last = line.findLastAnyOf(needles)?.second?.let(::toDigit)
+
+        requireNotNull(first)
+        requireNotNull(last)
+
+        return Pair(first, last)
+    }
+
+    private fun toDigit(candidate: String): Int =
+        digitLookupTable[candidate] ?: throw IllegalArgumentException("Failed to convert $candidate to a digit")
+}
--- day01/src/test/kotlin/de/nimelrian/aoc2023/MainKtTest.kt
+++ day01/src/test/kotlin/de/nimelrian/aoc2023/MainKtTest.kt
@@ -1,6 +1,7 @@
 package de.nimelrian.aoc2023
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class MainKtTest {
@@ -15,5 +16,32 @@ class MainKtTest {
         """.trimIndent()
         val returnValue = solve1(input.lineSequence())
         assertThat(returnValue).isEqualTo(142)
+    }
+
+    @Test
+    fun part1RegressionTest() {
+        val input = (javaClass.getResourceAsStream("/input")!!)
+            .bufferedReader()
+            .lineSequence()
+        val returnValue = solve1(input)
+        assertThat(returnValue).isEqualTo(55130)
+    }
+
+    @Nested
+    inner class Part2 {
+        @Test
+        fun part2() {
+            val input = """
+                two1nine
+                eightwothree
+                abcone2threexyz
+                xtwone3four
+                4nineeightseven2
+                zoneight234
+                7pqrstsixteen
+            """.trimIndent()
+            val returnValue = solve2(input.lineSequence())
+            assertThat(returnValue).isEqualTo(281)
+        }
     }
 }
